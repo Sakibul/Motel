e925114570df910ed77a389dc26b397e8155719f
SAKIBUL ISLAM KHAN <sakibul@live.com>
2020-05-10 08:35:40 -0400

    Room

--- Motel/MainWindow.xaml.cs
+++ Motel/MainWindow.xaml.cs
@@ -24,5 +24,59 @@ namespace Motel
         {
             InitializeComponent();
         }
+
+        private void btnRoomEdit_Click(object sender, RoutedEventArgs e)
+        {
+            service.RoomService roomService = new service.RoomService();
+            
+            Room room = new Room();
+            room.RoomId = Byte.Parse(txtRoomRoomId.Text);
+            room.Type = txtRoomType.Text;
+            room.NoOfBeds = Byte.Parse(txtRoomNoOfBeds.Text);
+            room.Availability = (chkRoomAvailability.IsChecked == true);
+            room.RatePerDay = Decimal.Parse(txtRoomRatePerDay.Text);
+
+            roomService.EditRoom(room);
+
+            MessageBox.Show("Room has been updated. Please reload Grid.");
+        }
+
+        private void btnRoomAdd_Click(object sender, RoutedEventArgs e)
+        {
+            service.RoomService roomService = new service.RoomService();
+
+            Room room = new Room()
+            {
+                Type = txtRoomType.Text,
+                NoOfBeds = Byte.Parse(txtRoomNoOfBeds.Text),
+                Availability = (chkRoomAvailability.IsChecked == true),
+                RatePerDay = Decimal.Parse(txtRoomRatePerDay.Text)
+            };
+
+            roomService.AddRoom(room);
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            service.RoomService roomService = new service.RoomService();
+            this.DataContext = roomService.GetAllRooms();
+        }
+
+        private void dataGridRoom_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            object item = dataGridRoom.SelectedItem;
+
+            txtRoomRoomId.Text = (dataGridRoom.SelectedCells[0].Column.GetCellContent(item) as TextBlock).Text;
+            txtRoomType.Text = (dataGridRoom.SelectedCells[1].Column.GetCellContent(item) as TextBlock).Text;
+            txtRoomNoOfBeds.Text = (dataGridRoom.SelectedCells[2].Column.GetCellContent(item) as TextBlock).Text;
+            //chkRoomAvailability.Text = (dataGridRoom.SelectedCells[3].Column.GetCellContent(item) as TextBlock).Text;
+            txtRoomRatePerDay.Text = (dataGridRoom.SelectedCells[4].Column.GetCellContent(item) as TextBlock).Text;
+        }
+
+        private void btnRoomLoadGrid_Click(object sender, RoutedEventArgs e)
+        {
+            service.RoomService roomService = new service.RoomService();
+            this.DataContext = roomService.GetAllRooms();
+        }
     }
 }
---
+++ Motel/service/RoomService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Motel.service
+{
+    class RoomService
+    {
+        public List<Room> GetAllRooms()
+        {
+            using (MotelDBEntities dbContext = new MotelDBEntities())
+            {
+                var query = from room in dbContext.Rooms
+                            select room;
+
+                return query.ToList<Room>();
+            }
+        }
+
+        public void AddRoom(Room room)
+        {
+            using (MotelDBEntities dbContext = new MotelDBEntities())
+            {
+                dbContext.Rooms.Add(room);
+                dbContext.SaveChanges();
+            }
+        }
+
+        public bool EditRoom(Room roomWithNewValues)
+        {
+            using (MotelDBEntities dbContext = new MotelDBEntities())
+            {
+                var query = from room in dbContext.Rooms
+                            where room.RoomId == roomWithNewValues.RoomId
+                            select room;
+
+                Room selectedRoom = query.First(); // I know the Room exists
+                // ***************************************
+                selectedRoom.Type = roomWithNewValues.Type;
+                selectedRoom.NoOfBeds = roomWithNewValues.NoOfBeds;
+                selectedRoom.Availability = roomWithNewValues.Availability;
+                selectedRoom.RatePerDay = roomWithNewValues.RatePerDay;
+                // ***************************************
+                // dbContext.Rooms.Remove(selectedCustomer);
+                dbContext.SaveChanges();
+                // ***************************************
+                // dbContext.Rooms.Add(selectedRoom);
+                // dbContext.SaveChanges();
+                // ***************************************
+            }
+            return true;
+        }
+    }
+}
